d3d9e7d99ef85519f069545eb433aadaf7d217eb
L3viathan <git@l3vi.de>
2020-06-01 11:04:38 +0200

    Docs, improve init, allow not showing dirty data

--- reproduci.py
+++ reproduci.py
@@ -31,19 +31,37 @@ def cli():
     pass
 
 
-@cli.command()
-def init():
-    for top_level_dir in ["sources", "workdata", "results", "scripts", "store"]:
-        os.makedirs(top_level_dir, exist_ok=True)
-    with open("Makefile", "w") as f:
-        f.write("")
+@cli.command(help="Initialize a reproduci project (create folders and do a `git init`)")
+@click.option(
+    "--all",
+    "-a",
+    "all_",
+    is_flag=True,
+    help="Create folders that are not always needed (writing, persistent)",
+)
+def init(all_):
+    folders = ["sources", "workdata", "outputs", "scripts"]
+    if all_:
+        folders.extend(["writing", "persistent"])
+    for folder in folders:
+        Path(folder).mkdir(exist_ok=True)
+    Path("Makefile").touch()
     assert subprocess.call(["git", "init"]) == 0
 
 
-@cli.command()
+@cli.command(
+    help="Store data in the internal store, annotated with timestamp, tag, and commit"
+)
 @click.argument("tag")
-@click.option("--tee", is_flag=True)
-@click.option("--allow-dirty", is_flag=True, default=True)
+@click.option(
+    "--tee", is_flag=True, help="Forward stdin to stdout in addition to storing"
+)
+@click.option(
+    "--allow-dirty/--no-allow-dirty",
+    is_flag=True,
+    default=True,
+    help="Allow storing of data even if Git is dirty.",
+)
 def store(tag, tee, allow_dirty):
     full_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
     short_hash = full_hash[:7]
@@ -72,11 +90,21 @@ def store(tag, tee, allow_dirty):
         raise click.ClickException("git dirty, saved as {}/{}".format(tag, filename))
 
 
-@cli.command()
+@cli.command(help="Retrieve data from the internal store")
 @click.argument("commit-spec", required=False)
-@click.option("--tag")
-@click.option("--raw", "-r", is_flag=True)
-def load(commit_spec, tag=None, raw=False):
+@click.option(
+    "--tag", help="A tag to filter for. If not given, shows results from all tags"
+)
+@click.option(
+    "--raw", "-r", is_flag=True, help="Don't print headers or seperating newlines"
+)
+@click.option(
+    "--show-dirty/--no-show-dirty",
+    is_flag=True,
+    default=True,
+    help="Allow results from dirty Git states to be shown",
+)
+def load(commit_spec, tag, raw, show_dirty):
     # try to find all results with that hash, and print them.
     # if a tag is given, filter on that tag
     if commit_spec is None:
@@ -85,23 +113,22 @@ def load(commit_spec, tag=None, raw=False):
         full_hash = (
             subprocess.check_output(["git", "rev-parse", commit_spec],).decode().strip()
         )
-    short_hash = full_hash[:7]
+        short_hash = full_hash[:7]
     if tag is None:
         tags = Path("store").glob("*")
     else:
         tags = [Path("store") / tag]
 
     for the_tag in tags:
-        if (
-            not tag and not raw
-        ):  # we are looking at several tags, therefore we print a header
-            print("[{}]".format(the_tag.name))
         candidate_iter = (
             the_tag.glob("*")
             if commit_spec == "all"
             else the_tag.glob("*-{}*".format(short_hash))
         )
         for candidate in candidate_iter:
+            buffer = []
+            if not raw:
+                buffer.append("[{}]\n".format(the_tag.name))
             with candidate.open() as f:
                 past_empty_line = False
                 for line in f:
@@ -109,15 +136,23 @@ def load(commit_spec, tag=None, raw=False):
                         past_empty_line = True
                         if raw:
                             continue
-                    if not past_empty_line and raw:
-                        continue
-                    sys.stdout.write(line)
-            if not raw:
-                print()
-                print()
+                    if not past_empty_line:
+                        key, _, val = line.partition(": ")
+                        if key == "Dirty" and val.strip() == "1" and not show_dirty:
+                            break
+                        if raw:
+                            continue
+                    buffer.append(line)
+                else:
+                    for line in buffer:
+                        sys.stdout.write(line)
+
+                    if not raw:
+                        print()
+                        print()
 
 
-@cli.command()
+@cli.command(help="Show some stats on the internal store")
 def stats():
     tags, commits = Counter(), Counter()
     dirty, total = 0, 0
