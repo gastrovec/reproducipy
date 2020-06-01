2b11f24ca7d9bf870de5ca18d8e97c7b395d38f4
L3viathan <git@l3vi.de>
2020-06-01 10:09:08 +0200

    Add switch to refuse storing dirty, add stats command

--- reproduci.py
+++ reproduci.py
@@ -3,6 +3,9 @@ import os
 import subprocess
 from datetime import datetime
 from pathlib import Path
+from collections import Counter
+from datetime import datetime
+from textwrap import dedent
 
 import click
 
@@ -40,13 +43,16 @@ def init():
 @cli.command()
 @click.argument("tag")
 @click.option("--tee", is_flag=True)
-def store(tag, tee):
+@click.option("--allow-dirty", is_flag=True, default=True)
+def store(tag, tee, allow_dirty):
     full_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
     short_hash = full_hash[:7]
     is_dirty, has_untracked = get_git_status()
     dt = datetime.now().astimezone()
     header = {"Hash": full_hash, "Timestamp": dt.strftime("%Y-%m-%dT%H:%M:%S.%f%z")}
     if is_dirty:
+        if not allow_dirty:
+            raise click.ClickException("git dirty, refusing to store")
         header["Dirty"] = 1
     if has_untracked:
         header["HasUntracked"] = 1
@@ -109,6 +115,38 @@ def load(commit_spec, tag=None, raw=False):
             if not raw:
                 print()
                 print()
+
+
+@cli.command()
+def stats():
+    tags, commits = Counter(), Counter()
+    dirty, total = 0, 0
+    max_date, min_date = None, None
+    for tag in Path("store").glob("*"):
+        for file in tag.glob("*"):
+            ts, _, rest = file.name.partition("-")
+            commit_hash, _, dirt = rest.partition("-")
+            dt = datetime.strptime(ts, "%Y%m%d%H%M%S")
+            if max_date is None or dt > max_date:
+                max_date = dt
+            if min_date is None or dt < min_date:
+                min_date = dt
+            commits[commit_hash] += 1
+            tags[tag.name] += 1
+            dirty += bool(dirt)
+            total += 1
+    top_commits = ", ".join(commit for commit, _ in commits.most_common(3))
+    top_tags = ", ".join(tag for tag, _ in tags.most_common(3))
+    print(
+        dedent(
+            f"""
+            Stored {total} results in total, {dirty} from dirty commits.
+            Earliest: {str(min_date)} Latest: {str(max_date)}
+            {len(tags)} tags in total, most common: {top_tags}
+            {len(commits)} commits in total, most common: {top_commits}
+            """
+        ).strip("\n")
+    )
 
 
 if __name__ == "__main__":
